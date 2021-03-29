c272fd35056f508ce6b7157b7f181a8de5907bc5
yutanakamura-tky <yutanakamura.tky@gmail.com>
2021-03-29 19:05:41 +0900

    NER-02: test for NCBI Brat -> XML

---
+++ tests/modules/test_brat_to_xml.py
@@ -0,0 +1,21 @@
+import pathlib
+
+import bs4
+
+from biomedical_ner.src.modules import brat_to_xml
+
+BASE_DIR = pathlib.Path(__file__).resolve().parent
+
+
+def test_compose_xml_works_correctly():
+    with open(BASE_DIR / "ncbi_disease_corpus_sample.txt") as f:
+        text_sample = f.read()
+    with open(BASE_DIR / "ncbi_disease_corpus_sample.xml") as f:
+        expected_xml_text = f.read()
+
+    document = brat_to_xml.convert_text_to_document_obj(text_sample)
+    xml_text = brat_to_xml.compose_xml([document])
+    xml = bs4.BeautifulSoup(xml_text, "xml").prettify()
+    expected_xml = bs4.BeautifulSoup(expected_xml_text, "xml").prettify()
+
+    assert xml == expected_xml
