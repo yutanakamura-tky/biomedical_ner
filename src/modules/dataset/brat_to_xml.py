b503896463e9f5875032d24e04a083adc86211d7
yutanakamura-tky <yutanakamura.tky@gmail.com>
2021-04-02 08:36:59 +0900

    NER-03: BratToXmlConverter

--- src/modules/dataset/brat_to_xml.py
+++ src/modules/dataset/brat_to_xml.py
@@ -19,48 +19,10 @@ def main():
         input_path = NCBI_DIR / f"NCBI{corpus_name}set_corpus.txt"
         output_path = pathlib.Path(input_path).with_suffix(".xml")
 
-        xml_text = convert_brat_txt_to_xml_text(input_path)
+        xml_text = BratToXmlConverter.convert_brat_txt_to_xml_text(input_path)
         with open(output_path, "w") as f:
             f.write(xml_text)
         print(f"XML written to {output_path}")
-
-
-def convert_brat_txt_to_xml_text(input_path: str) -> str:
-    """
-    Opens convert NER annotation in Brat style and convert it into XML text
-    Input file must satisfy the following:
-
-        1. Starts with zero or one brank line
-        2. Starts with zero or one brank line
-        3. Documents are separated with a brank line
-        4. The first line of each document is like below:
-            f"{ID}|t|{TEXT}"
-        5. The second line of each document is like below:
-            f"{ID}|a|{TEXT}"
-        6. The rest lines of each document are like below:
-            f"{ID}\t{BEGIN_POSITION}\t{END_POSITION}\t{ENTITY}\t{ENTITY_TYPE}\t{MESH_TAG}"
-    """
-    with open(input_path) as f:
-        print(f"Opening {input_path} ...")
-        lines = clean_brat_txt(f.readlines())
-
-    # split records (separated with a brank line)
-    raw_documents = "".join(lines).split("\n\n")
-
-    documents = [convert_text_to_document_obj(doc) for doc in tqdm(raw_documents)]
-    xml_text = compose_xml(documents)
-
-    return xml_text
-
-
-def clean_brat_txt(lines: List[str]) -> List[str]:
-    # If a file starts with a brank line, remove it
-    lines = lines[1:] if lines[0] == "\n" else lines
-
-    # If a file ends with a brank line, remove it
-    lines = lines[:-1] if lines[-1] == "\n" else lines
-
-    return lines
 
 
 @dataclasses.dataclass
@@ -87,140 +49,183 @@ class Document:
     entities: List[Entity]
 
 
-def convert_text_to_document_obj(document: str) -> Document:
-    doc_lines = document.split("\n")
-    # Extract PMID from the beginning of each document
-    # e.g., '12345678|t|...' -> '12345678'
-    pmid = re.match(r"^\d+", doc_lines[0]).group(0)
+class BratToXmlConverter:
+    @classmethod
+    def convert_brat_txt_to_xml_text(cls, input_path: str) -> str:
+        """
+        Opens convert NER annotation in Brat style and convert it into XML text
+        Input file must satisfy the following:
 
-    # '12345678|t|foo' -> 'foo'
-    title = re.match(r"^\d+\|t\|(.+)", doc_lines[0]).group(1).strip()
+            1. Starts with zero or one brank line
+            2. Starts with zero or one brank line
+            3. Documents are separated with a brank line
+            4. The first line of each document is like below:
+                f"{ID}|t|{TEXT}"
+            5. The second line of each document is like below:
+                f"{ID}|a|{TEXT}"
+            6. The rest lines of each document are like below:
+                f"{ID}\t{BEGIN_POSITION}\t{END_POSITION}\t{ENTITY}\t{ENTITY_TYPE}\t{MESH_TAG}"
+        """
+        with open(input_path) as f:
+            print(f"Opening {input_path} ...")
+            lines = cls.clean_brat_txt(f.readlines())
 
-    # '12345678|a|foo' -> 'foo'
-    article = re.match(r"^\d+\|a\|(.+)", doc_lines[1]).group(1).strip()
+        # split records (separated with a brank line)
+        raw_documents = "".join(lines).split("\n\n")
 
-    content = f"{title} {article}"
+        documents = [
+            cls.convert_text_to_document_obj(doc) for doc in tqdm(raw_documents)
+        ]
+        xml_text = cls.compose_xml(documents)
 
-    raw_annotations = doc_lines[2:]
-    annotations = []
+        return xml_text
 
-    for annotation in raw_annotations:
-        annotation_values = annotation.split("\t")
-        if len(annotation_values) == 6:
-            _pmid, start, end, value, entity_type, mesh = annotation_values
-            entity = Entity(
-                start=int(start),
-                end=int(end),
-                value=value,
-                entity_type=entity_type,
-                mesh=mesh,
-            )
-            annotations.append(entity)
+    @staticmethod
+    def clean_brat_txt(lines: List[str]) -> List[str]:
+        # If a file starts with a brank line, remove it
+        lines = lines[1:] if lines[0] == "\n" else lines
+
+        # If a file ends with a brank line, remove it
+        lines = lines[:-1] if lines[-1] == "\n" else lines
+
+        return lines
+
+    @staticmethod
+    def convert_text_to_document_obj(document: str) -> Document:
+        doc_lines = document.split("\n")
+        # Extract PMID from the beginning of each document
+        # e.g., '12345678|t|...' -> '12345678'
+        pmid = re.match(r"^\d+", doc_lines[0]).group(0)
+
+        # '12345678|t|foo' -> 'foo'
+        title = re.match(r"^\d+\|t\|(.+)", doc_lines[0]).group(1).strip()
+
+        # '12345678|a|foo' -> 'foo'
+        article = re.match(r"^\d+\|a\|(.+)", doc_lines[1]).group(1).strip()
+
+        content = f"{title} {article}"
+
+        raw_annotations = doc_lines[2:]
+        annotations = []
+
+        for annotation in raw_annotations:
+            annotation_values = annotation.split("\t")
+            if len(annotation_values) == 6:
+                _pmid, start, end, value, entity_type, mesh = annotation_values
+                entity = Entity(
+                    start=int(start),
+                    end=int(end),
+                    value=value,
+                    entity_type=entity_type,
+                    mesh=mesh,
+                )
+                annotations.append(entity)
+            else:
+                # Skip lines without entity in BC5CDR corpus
+                # e.g., '12345678 CID Dxxxxxx Dyyyyyy ...'
+                continue
+
+        result = Document(pmid=int(pmid), content=content, entities=annotations)
+        return result
+
+    @classmethod
+    def compose_xml(cls, documents: List[Document]) -> str:
+        xml_texts = []
+        n_entity = []
+
+        for document in tqdm(documents):
+            xml_texts.append(cls.compose_xml_document_tag(document))
+            n_entity.append(cls.count_entities(document))
+
+        header = f'<xml version="1.0" encoding="UTF-8"?>\n<dataset n_documents="{len(documents)}" n_entity="{sum(n_entity)}">\n'
+        footer = "</dataset>"
+        xml_text = header + "".join(xml_texts) + footer
+
+        return xml_text
+
+    @classmethod
+    def compose_xml_document_tag(cls, document: Document) -> str:
+        disease_tags = {}
+        for entity in document.entities:
+            if entity.start not in disease_tags.keys():
+                disease_tags[entity.start] = {
+                    "opening_tags": [entity.opening_tag],
+                    "closing_tags": [],
+                }
+
+            else:
+                disease_tags[entity.start]["opening_tags"].append(entity.opening_tag)
+
+            if entity.end not in disease_tags.keys():
+                disease_tags[entity.end] = {
+                    "opening_tags": [],
+                    "closing_tags": [entity.closing_tag],
+                }
+            else:
+                disease_tags[entity.end]["closing_tags"].append(entity.closing_tag)
+
+        n_entity = cls.count_entities(document)
+        header = f'<document id="{document.pmid}" n_entity="{n_entity}">\n'
+        footer = "\n</document>\n"
+
+        xml_text = header
+
+        for i in range(len(document.content)):
+            # Add letters to XML text one by one
+            if i in disease_tags.keys():
+                # When comming to entity position, add tags to XML text
+
+                # Closing tags have to be added in inverted order to avoid tag crossing
+                # e.g.,
+                #     (text)
+                #     12345678 10 22 entity_1 type_1 mesh_1
+                #     12345678 15 22 entity_2 type_2 mesh_2
+                #
+                #     (disease_tags)
+                #     {10:{'opening_tags':['opening_tag_1'], 'closing_tags':[]},
+                #      15:{'opening_tags':['opening_tag_2'], 'closing_tags':[]},
+                #      22:{'opening_tags':[], 'closing_tags':['closing_tag_1', 'closing_tag_2']}}
+                #
+                #     -> 'closing_tag_2' must be come before 'closing_tag_1'
+
+                xml_text += "".join(disease_tags[i]["closing_tags"][::-1])
+
+                # Opening tags have to be added in inverted order to avoid tag crossing
+                # e.g.,
+                #     (text)
+                #     12345678 40 45 entity_1 type_1 mesh_1
+                #     12345678 40 53 entity_2 type_2 mesh_2
+                #
+                #     (disease_tags)
+                #     {40:{'opening_tags':['opening_tag_1', 'opening_tag_2'], 'closing_tags':[]},
+                #      45:{'opening_tags':[], 'closing_tags':['closing_tag_1']},
+                #      53:{'opening_tags':[], 'closing_tags':['closing_tag_2']}}
+                #
+                #     -> 'opening_tag_2' must be come before 'opening_tag_1'
+
+                xml_text += "".join(disease_tags[i]["opening_tags"][::-1])
+
+            # Convert letters <, >, ', " to avoid XML parsing errors
+            xml_text += cls.convert_letters(document.content[i])
+        xml_text += footer
+        return xml_text
+
+    @staticmethod
+    def count_entities(document: Document) -> int:
+        return len(document.entities)
+
+    @staticmethod
+    def convert_letters(letter: str) -> str:
+        if letter not in ("'", '"', "<", ">"):
+            return letter
         else:
-            # Skip lines without entity in BC5CDR corpus
-            # e.g., '12345678 CID Dxxxxxx Dyyyyyy ...'
-            continue
-
-    result = Document(pmid=int(pmid), content=content, entities=annotations)
-    return result
-
-
-def compose_xml(documents: List[Document]) -> str:
-    xml_texts = []
-    n_entity = []
-
-    for document in tqdm(documents):
-        xml_texts.append(compose_xml_document_tag(document))
-        n_entity.append(count_entities(document))
-
-    header = f'<xml version="1.0" encoding="UTF-8"?>\n<dataset n_documents="{len(documents)}" n_entity="{sum(n_entity)}">\n'
-    footer = "</dataset>"
-    xml_text = header + "".join(xml_texts) + footer
-
-    return xml_text
-
-
-def compose_xml_document_tag(document: Document) -> str:
-    disease_tags = {}
-    for entity in document.entities:
-        if entity.start not in disease_tags.keys():
-            disease_tags[entity.start] = {
-                "opening_tags": [entity.opening_tag],
-                "closing_tags": [],
+            mapping = {
+                "'": "&apos;",
+                '"': "&quot;",
+                "<": "&lt;",
+                ">": "&gt;",
             }
-
-        else:
-            disease_tags[entity.start]["opening_tags"].append(entity.opening_tag)
-
-        if entity.end not in disease_tags.keys():
-            disease_tags[entity.end] = {
-                "opening_tags": [],
-                "closing_tags": [entity.closing_tag],
-            }
-        else:
-            disease_tags[entity.end]["closing_tags"].append(entity.closing_tag)
-
-    header = f'<document id="{document.pmid}" n_entity="{count_entities(document)}">\n'
-    footer = "\n</document>\n"
-
-    xml_text = header
-
-    for i in range(len(document.content)):
-        # Add letters to XML text one by one
-        if i in disease_tags.keys():
-            # When comming to entity position, add tags to XML text
-
-            # Closing tags have to be added in inverted order to avoid tag crossing
-            # e.g.,
-            #     (text)
-            #     12345678 10 22 entity_1 type_1 mesh_1
-            #     12345678 15 22 entity_2 type_2 mesh_2
-            #
-            #     (disease_tags)
-            #     {10:{'opening_tags':['opening_tag_1'], 'closing_tags':[]},
-            #      15:{'opening_tags':['opening_tag_2'], 'closing_tags':[]},
-            #      22:{'opening_tags':[], 'closing_tags':['closing_tag_1', 'closing_tag_2']}}
-            #
-            #     -> 'closing_tag_2' must be come before 'closing_tag_1'
-
-            xml_text += "".join(disease_tags[i]["closing_tags"][::-1])
-
-            # Opening tags have to be added in inverted order to avoid tag crossing
-            # e.g.,
-            #     (text)
-            #     12345678 40 45 entity_1 type_1 mesh_1
-            #     12345678 40 53 entity_2 type_2 mesh_2
-            #
-            #     (disease_tags)
-            #     {40:{'opening_tags':['opening_tag_1', 'opening_tag_2'], 'closing_tags':[]},
-            #      45:{'opening_tags':[], 'closing_tags':['closing_tag_1']},
-            #      53:{'opening_tags':[], 'closing_tags':['closing_tag_2']}}
-            #
-            #     -> 'opening_tag_2' must be come before 'opening_tag_1'
-
-            xml_text += "".join(disease_tags[i]["opening_tags"][::-1])
-
-        # Convert letters <, >, ', " to avoid XML parsing errors
-        xml_text += convert_letters(document.content[i])
-    xml_text += footer
-    return xml_text
-
-
-def count_entities(document: Document) -> int:
-    return len(document.entities)
-
-
-def convert_letters(letter: str) -> str:
-    if letter not in ("'", '"', "<", ">"):
-        return letter
-    else:
-        mapping = {
-            "'": "&apos;",
-            '"': "&quot;",
-            "<": "&lt;",
-            ">": "&gt;",
-        }
-        return mapping[letter]
+            return mapping[letter]
 
 
 if __name__ == "__main__":
--- tests/modules/dataset/test_brat_to_xml.py
+++ tests/modules/dataset/test_brat_to_xml.py
@@ -2,19 +2,18 @@ import pathlib
 
 import bs4
 
-from biomedical_ner.src.modules import brat_to_xml
+from biomedical_ner.src.modules.dataset.brat_to_xml import BratToXmlConverter
 
 BASE_DIR = pathlib.Path(__file__).resolve().parent
 
 
 def test_compose_xml_works_correctly():
-    with open(BASE_DIR / "sample_ncbi_disease_corpus.txt") as f:
-        text_sample = f.read()
-    with open(BASE_DIR / "sample_ncbi_disease_corpus.xml") as f:
+    text_sample_path = BASE_DIR / "sample_ncbi_disease_corpus.txt"
+    xml_sample_path = BASE_DIR / "sample_ncbi_disease_corpus.xml"
+    with open(xml_sample_path) as f:
         expected_xml_text = f.read()
 
-    document = brat_to_xml.convert_text_to_document_obj(text_sample)
-    xml_text = brat_to_xml.compose_xml([document])
+    xml_text = BratToXmlConverter.convert_brat_txt_to_xml_text(text_sample_path)
     xml = bs4.BeautifulSoup(xml_text, "xml").prettify()
     expected_xml = bs4.BeautifulSoup(expected_xml_text, "xml").prettify()
 
