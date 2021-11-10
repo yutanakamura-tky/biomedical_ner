66f7bab1d08c4a4dd340a77a88145ea5de8f5785
yutanakamura-tky <yutanakamura.tky@gmail.com>
2021-11-11 01:25:08 +0900

    NER-07: created MedNLI script

---
+++ src/modules/mednli/check_performance_of_bert.sh
@@ -0,0 +1,44 @@
+python mednli.py --bert-path ~/kart/models/tf_bert_scratch_hospital_c0p2_no_anonymization/pretraining_output_stage1/model.ckpt-1000000 --lr 5e-5 --max-epoch 4 --experiment-name mednli_bert_no_anonym
+python mednli.py --bert-path ~/kart/models/tf_bert_scratch_hospital_c0p2_no_anonymization/pretraining_output_stage1/model.ckpt-1000000 --lr 5e-5 --max-epoch 3 --experiment-name mednli_bert_no_anonym
+python mednli.py --bert-path ~/kart/models/tf_bert_scratch_hospital_c0p2_no_anonymization/pretraining_output_stage1/model.ckpt-1000000 --lr 5e-5 --max-epoch 2 --experiment-name mednli_bert_no_anonym
+python mednli.py --bert-path ~/kart/models/tf_bert_scratch_hospital_c0p2_no_anonymization/pretraining_output_stage1/model.ckpt-1000000 --lr 4e-5 --max-epoch 4 --experiment-name mednli_bert_no_anonym
+python mednli.py --bert-path ~/kart/models/tf_bert_scratch_hospital_c0p2_no_anonymization/pretraining_output_stage1/model.ckpt-1000000 --lr 4e-5 --max-epoch 3 --experiment-name mednli_bert_no_anonym
+python mednli.py --bert-path ~/kart/models/tf_bert_scratch_hospital_c0p2_no_anonymization/pretraining_output_stage1/model.ckpt-1000000 --lr 4e-5 --max-epoch 2 --experiment-name mednli_bert_no_anonym
+python mednli.py --bert-path ~/kart/models/tf_bert_scratch_hospital_c0p2_no_anonymization/pretraining_output_stage1/model.ckpt-1000000 --lr 3e-5 --max-epoch 4 --experiment-name mednli_bert_no_anonym
+python mednli.py --bert-path ~/kart/models/tf_bert_scratch_hospital_c0p2_no_anonymization/pretraining_output_stage1/model.ckpt-1000000 --lr 3e-5 --max-epoch 3 --experiment-name mednli_bert_no_anonym
+python mednli.py --bert-path ~/kart/models/tf_bert_scratch_hospital_c0p2_no_anonymization/pretraining_output_stage1/model.ckpt-1000000 --lr 3e-5 --max-epoch 2 --experiment-name mednli_bert_no_anonym
+python mednli.py --bert-path ~/kart/models/tf_bert_scratch_hospital_c0p2_no_anonymization/pretraining_output_stage1/model.ckpt-1000000 --lr 2e-5 --max-epoch 4 --experiment-name mednli_bert_no_anonym
+python mednli.py --bert-path ~/kart/models/tf_bert_scratch_hospital_c0p2_no_anonymization/pretraining_output_stage1/model.ckpt-1000000 --lr 2e-5 --max-epoch 3 --experiment-name mednli_bert_no_anonym
+python mednli.py --bert-path ~/kart/models/tf_bert_scratch_hospital_c0p2_no_anonymization/pretraining_output_stage1/model.ckpt-1000000 --lr 2e-5 --max-epoch 2 --experiment-name mednli_bert_no_anonym
+
+
+
+
+python mednli.py --bert-path ~/kart/models/tf_bert_scratch_hospital_c0p2_hipaa/pretraining_output_stage1/model.ckpt-1000000 --lr 5e-5 --max-epoch 4 --experiment-name mednli_bert_hipaa
+python mednli.py --bert-path ~/kart/models/tf_bert_scratch_hospital_c0p2_hipaa/pretraining_output_stage1/model.ckpt-1000000 --lr 5e-5 --max-epoch 3 --experiment-name mednli_bert_hipaa
+python mednli.py --bert-path ~/kart/models/tf_bert_scratch_hospital_c0p2_hipaa/pretraining_output_stage1/model.ckpt-1000000 --lr 5e-5 --max-epoch 2 --experiment-name mednli_bert_hipaa
+python mednli.py --bert-path ~/kart/models/tf_bert_scratch_hospital_c0p2_hipaa/pretraining_output_stage1/model.ckpt-1000000 --lr 4e-5 --max-epoch 4 --experiment-name mednli_bert_hipaa
+python mednli.py --bert-path ~/kart/models/tf_bert_scratch_hospital_c0p2_hipaa/pretraining_output_stage1/model.ckpt-1000000 --lr 4e-5 --max-epoch 3 --experiment-name mednli_bert_hipaa
+python mednli.py --bert-path ~/kart/models/tf_bert_scratch_hospital_c0p2_hipaa/pretraining_output_stage1/model.ckpt-1000000 --lr 4e-5 --max-epoch 2 --experiment-name mednli_bert_hipaa
+python mednli.py --bert-path ~/kart/models/tf_bert_scratch_hospital_c0p2_hipaa/pretraining_output_stage1/model.ckpt-1000000 --lr 3e-5 --max-epoch 4 --experiment-name mednli_bert_hipaa
+python mednli.py --bert-path ~/kart/models/tf_bert_scratch_hospital_c0p2_hipaa/pretraining_output_stage1/model.ckpt-1000000 --lr 3e-5 --max-epoch 3 --experiment-name mednli_bert_hipaa
+python mednli.py --bert-path ~/kart/models/tf_bert_scratch_hospital_c0p2_hipaa/pretraining_output_stage1/model.ckpt-1000000 --lr 3e-5 --max-epoch 2 --experiment-name mednli_bert_hipaa
+python mednli.py --bert-path ~/kart/models/tf_bert_scratch_hospital_c0p2_hipaa/pretraining_output_stage1/model.ckpt-1000000 --lr 2e-5 --max-epoch 4 --experiment-name mednli_bert_hipaa
+python mednli.py --bert-path ~/kart/models/tf_bert_scratch_hospital_c0p2_hipaa/pretraining_output_stage1/model.ckpt-1000000 --lr 2e-5 --max-epoch 3 --experiment-name mednli_bert_hipaa
+python mednli.py --bert-path ~/kart/models/tf_bert_scratch_hospital_c0p2_hipaa/pretraining_output_stage1/model.ckpt-1000000 --lr 2e-5 --max-epoch 2 --experiment-name mednli_bert_hipaa
+
+
+
+
+python mednli.py --lr 5e-5 --max-epoch 4 --experiment-name mednli_bert_general
+python mednli.py --lr 5e-5 --max-epoch 3 --experiment-name mednli_bert_general
+python mednli.py --lr 5e-5 --max-epoch 2 --experiment-name mednli_bert_general
+python mednli.py --lr 4e-5 --max-epoch 4 --experiment-name mednli_bert_general
+python mednli.py --lr 4e-5 --max-epoch 3 --experiment-name mednli_bert_general
+python mednli.py --lr 4e-5 --max-epoch 2 --experiment-name mednli_bert_general
+python mednli.py --lr 3e-5 --max-epoch 4 --experiment-name mednli_bert_general
+python mednli.py --lr 3e-5 --max-epoch 3 --experiment-name mednli_bert_general
+python mednli.py --lr 3e-5 --max-epoch 2 --experiment-name mednli_bert_general
+python mednli.py --lr 2e-5 --max-epoch 4 --experiment-name mednli_bert_general
+python mednli.py --lr 2e-5 --max-epoch 3 --experiment-name mednli_bert_general
+python mednli.py --lr 2e-5 --max-epoch 2 --experiment-name mednli_bert_general
